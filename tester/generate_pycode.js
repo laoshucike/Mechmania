096d3b3357b623f033507ad4745e9391b455966a
plantvsbirds <plantvsbird@gmail.com>
2016-10-01 14:36:52 -0500

    prefix for generation

--- tester/generate_pycode.js
+++ tester/generate_pycode.js
@@ -5,16 +5,17 @@ const fs = require('fs')
 let execOut = (cmd) => exec(cmd, {async : false, silent : true}).stdout.split('\n').filter(s => s.length > 0)
 
 
-exports.do = (base, team) => {
+exports.do = (base, team, prefix) => {
+  if (!prefix) prefix = '';
   var Injection = '[' + team.split('_').map(name => `{"CharacterName": "${name}}","ClassId": "${name}"}`) + ']'
-  exec(`rm -rf ../clients/${team}`)
-  exec(`cp -av ../clients/base ../clients/${team}`, {silent : true})
+  exec(`rm -rf ../clients/${prefix + team}`)
+  exec(`cp -av ../clients/${base} ../clients/${prefix + team}`, {silent : true})
   
-  lines = (execOut(`cat ../clients/${team}/client.py`))
+  lines = (execOut(`cat ../clients/${prefix + team}/client.py`))
   lines = lines.map(line => {
     return line.replace(replaceTag, Injection).replace(repalceNameTag, `'${team}'`)
   })
 
-  fs.writeFileSync(`../clients/${team}/client.py`, lines.join('\n'))
+  fs.writeFileSync(`../clients/${prefix + team}/client.py`, lines.join('\n'))
   return team + ' done!!'
 }
