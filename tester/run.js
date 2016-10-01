f751a1e52f1ebfd9156e3443dee0c3922ce14d55
plantvsbirds <plantvsbird@gmail.com>
2016-10-01 01:32:55 -0500

    trigger simulation

--- tester/run.js
+++ tester/run.js
@@ -3,4 +3,8 @@ let generate_pycode = require('./generate_pycode')
 let run_code = require('./run_code')
 
 
-run_code.do();
+let log = (msg) => console.log('[!!!!!]  ' + msg)
+
+if (run_code.do()) {
+  log('simulate success')
+}
--- tester/run_code.js
+++ tester/run_code.js
@@ -1,3 +1,11 @@
+require('shelljs/global');
+
 exports.do = function () {
-  console.log('hello')
+  var run = (cmd) => exec(cmd, {
+    async:false,
+    silent: true
+  })
+  run('cd ..')
+  var result = run('python2 ./gamerunner.py')
+  return (result.stdout.indexOf('Game finished - writing log to file') > -1)
 }
